e48bc0267c7377ad7e05729eeda4f211599f83b2
beth deutmeyer <bethdeutmeyer@gmail.com>
2023-09-25 18:11:00 -0500

    Render fixed, README completed

--- assets/js/highscorescript.js
+++ assets/js/highscorescript.js
@@ -1,40 +1,28 @@
 var highScoreUl = document.getElementById("leaderboard");
 var clearScores = document.getElementById("clear");
-var initials = [];
-var score = [];
+var comboList = [];
+var scoreBoard = JSON.parse(localStorage.getItem("initials")) || [];
+var initials;
+var score;
 
-//Starts when high score page loads
-function initializeHSPage() {
-        
-        var storedInitials = JSON.parse(localStorage.getItem("initials"));
-        var storedScore = JSON.parse(localStorage.getItem("score"));
-        // If initials and scores existed in the local storage, update arrays to stored lists
-        if (storedInitials !== null) {
-          initials = storedInitials;
-        }
-        if (storedScore !== null) {
-            score = storedScore;
-        }
-        renderScores();
-}
 
-//Renders initials and scores onto the page
+//Starts when high score page loads, renders score(s) onto page
 function renderScores() {
     highScoreUl.innerHTML = "";
-    //Renders a new li for each score added
-    for (var i = 0; i < initials.length; i++) {
-        var scoreboardInfo = initials[i] + ": " + score[i];
+    for (var i = 0; i < scoreBoard.length; i++) {
         var li = document.createElement("li");
-        li.textContent = scoreboardInfo;
-        li.setAttribute("data-index", i);
+        li.textContent = scoreBoard[i].initials + ": " + scoreBoard[i].score;
         highScoreUl.appendChild(li);
-        console.log(li.textContent);
-      }    
+        scoreBoard.sort(function (a, b) {
+            return b.score - a.score;
+        });
+    }  
 }
 
+//Clears scoreboard
 clearScores.addEventListener("click", function() {
     localStorage.clear();
     location.reload();
 });
 
-initializeHSPage();
+renderScores();
